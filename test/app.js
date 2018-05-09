934c99ca140ade15069c784bb863b49f280e7127
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-05-09 19:40:58 -0400

    change to test/app.js

--- test/app.js
+++ test/app.js
@@ -14,15 +14,15 @@ module.exports = function(app)
 
     //Original Code
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
-
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
-    }
+    // var connectionString = 'mongodb://127.0.0.1:27017/test';
+    //
+    // if(process.env.MLAB_USERNAME) {
+    //     connectionString = process.env.MLAB_USERNAME + ":" +
+    //         process.env.MLAB_PASSWORD + "@" +
+    //         process.env.MLAB_HOST + ':' +
+    //         process.env.MLAB_PORT + '/' +
+    //         process.env.MLAB_APP_NAME;
+    // }
 
     // Original Code ends
 
