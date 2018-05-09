dfbc54188355686fd5e4eb75a2453a7559136273
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-05-09 19:18:16 -0400

    changes to test/app.js

--- test/app.js
+++ test/app.js
@@ -14,27 +14,27 @@ module.exports = function(app)
 
     //Original Code
 
-    // var connectionString = 'mongodb://127.0.0.1:27017/test';
-    //
-    // if(process.env.MLAB_USERNAME) {
-    //     connectionString = process.env.MLAB_USERNAME + ":" +
-    //         process.env.MLAB_PASSWORD + "@" +
-    //         process.env.MLAB_HOST + ':' +
-    //         process.env.MLAB_PORT + '/' +
-    //         process.env.MLAB_APP_NAME;
-    // }
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
+
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
 
     // Original Code ends
 
     // Edited Code
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    	var username =  process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    	var password = process.env.MLAB_PASSWORD_WEBDEV;
-    	connectionString = 'mongodb://' + username + ':' + password;
-    	connectionString += '@ds119820.mlab.com:19820/heroku_v7xp8bm1'; // user yours
-    }
+    // var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    // if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    // 	var username =  process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    // 	var password = process.env.MLAB_PASSWORD_WEBDEV;
+    // 	connectionString = 'mongodb://' + username + ':' + password;
+    // 	connectionString += '@ds119820.mlab.com:19820/heroku_v7xp8bm1'; // user yours
+    // }
 
 
     var mongoose = require("mongoose");
